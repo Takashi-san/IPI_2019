266d463fac8699df21d2812c30298351a71599a0
Unknown <bt.tengan@gmail.com>
2019-04-11 22:43:14 -0300

    Consegue a versão equalizada do mapa

--- trabalho1/ipi_t1_q2.py
+++ trabalho1/ipi_t1_q2.py
@@ -5,3 +5,22 @@
 
 import cv2 as cv
 import numpy as np
+
+def bgr_to_gray(img):
+	#output = img.astype(int).sum(axis=2)/3
+	output = img[:,:,0].astype(int) + img[:,:,1].astype(int) + img[:,:,2].astype(int)
+	output = output//3
+	return output.astype(np.uint8)
+
+MRGB = cv.imread("img/Mars.bmp")
+
+MGray = bgr_to_gray(MRGB)
+
+Mheq = cv.equalizeHist(MGray)
+
+
+cv.imshow('ori', MRGB)
+cv.imshow('hnd', MGray)
+cv.imshow('heq', Mheq)
+cv.waitKey(0)
+cv.destroyAllWindows()
